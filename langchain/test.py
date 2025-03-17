05284ccaf12a25fdb5e2eb71c8675e2368307ce5
JuniorSummer <53287966+JuniorSummer@users.noreply.github.com>
2025-03-17 11:07:36 +0800

    Update test.py

--- langchain/test.py
+++ langchain/test.py
@@ -1,1 +1,18 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 
+loader = PyPDFLoader("/root/llm/28-2024年浙江省特殊食品监督抽检分析报告-提交版.pdf")
+pages = loader.load_and_split()
+# 查看加载后的文档
+print(pages)
+
+docs = ""
+for item in pages:
+    docs += item.page_content
+    docs + "\n"
+
+embeddings_path = "/root/packages/m3e-small/"
+embeddings = HuggingFaceEmbeddings(model_name=embeddings_path)
+vectorStoreDB = FAISS.from_documents(pages, embedding=embeddings)
+print(vectorStoreDB)
