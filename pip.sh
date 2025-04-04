0d83871590b2ea279e228e49d333dc58ff7864d6
JuniorSummer <53287966+JuniorSummer@users.noreply.github.com>
2025-04-04 20:35:44 +0800

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -2,5 +2,12 @@ pip freeze > pip_requirements.txt
 # 可以直接创建conda环境：conda create --name <env> --file <this file>
 conda list -e > conda_requirements.txt
 
+# 查看pip下载的缓存
+pip cache dir
 # 清空pip缓存
 pip cache purge
+
+# 查看conda下载的缓存
+conda clean --all --dry-run
+# 清空conda下载的缓存
+conda clean --tarballs
