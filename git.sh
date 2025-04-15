4b98efb89f7c31efd3823fe31e38f1421b939cdd
JuniorSummer <53287966+JuniorSummer@users.noreply.github.com>
2025-04-15 21:20:13 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,8 @@
 git config --global user.email=xiayuancheng333@163.com
 git config --global user.name=spike
 
+# 查看全局配置
 git config --list
+# 清除全局配置
+git config --global --unset user.name
+git config --global --unset user.email
