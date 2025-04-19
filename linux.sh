8272f3aba6f6197ed058c427aafa82c4e4d37ac0
JuniorSummer <53287966+JuniorSummer@users.noreply.github.com>
2025-04-19 13:45:54 +0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -9,3 +9,7 @@ df -hl
 # 删除容器和镜像
 docker rm -f CONTAINER ID
 docker rmi -f IMAGE ID
+# 保存镜像
+docker commit 7ca736d99653 yolov5:v6.2
+docker save -o yolov5-v6.2.tar yolov5:v6.2
+docker load -i yolov5-v6.2.tar
